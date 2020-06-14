81eb8fc6b78447b4b248df4455190631bd328433
Groeneveld <pim.groeneveld@capgemini.com>
2020-06-14 19:43:27 +0200

    mock test added and passing

---
+++ src/test/java/com/logius/digid/weatherproxy/CityEntityTest.java
@@ -0,0 +1,28 @@
+package com.logius.digid.weatherproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CityEntityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnRotterdamFromWeatherApi() throws Exception {
+        this.mockMvc.perform(get("/weatherproxy/cities/Rotterdam")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("minTemp\":289.82,\"maxTemp\":292.59,\"sunrise\":1592104928,\"id\":2747891,\"name\":\"Rotterdam")));
+    }
+
+}
